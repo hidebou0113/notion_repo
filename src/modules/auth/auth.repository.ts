b4185a9d199fc441f0f923b964f4da8ddf215423
hideyuki <hidesnow0113@gmail.com>
2026-07-11 13:45:20 +0900

    ログインAPIを叩く処理の追加

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -17,4 +17,18 @@ export const authRepository = {
     // ただのオブジェクトだった userがUserクラスのインスタンスになる
     return { user: new User(user), token };
   },
+  // 非同期処理 ログインAPIの呼び出し
+  async signin(
+    //signinの引数 文字列
+    email: string,
+    password: string,
+    // 返り値の型 Userの情報と文字列のトークンが返ってくる
+  ): Promise<{ user: User; token: string }> {
+    // ログイン用APiにPOSTリクエストを送信、awaitだからサーバーからの返事が来るまで待つ。
+    const result = await api.post('/auth/signin', { email, password });
+    // result.dataからuserとtokenの情報を取り出す。
+    const { user, token } = result.data;
+    // 呼び出し元にuserとtokenを返す。Userクラスのインスタンスにしてから返す
+    return { user: new User(user), token };
+  },
 };
