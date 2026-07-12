5039ad4d8c90d3f68639a12867b03714ebc1b98f
hidebou0113 <wakakinhidesnow920@gmail.com>
2026-07-12 16:21:19 +0900

    ホーム画面で作成したノートを一覧に反映

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,12 @@ import { FiPlus } from 'react-icons/fi';
 import '../styles/pages/home.css';
 import { useState } from 'react';
 import { noteRepository } from '../modules/notes/note.repository';
+import { useNoteStore } from '../modules/notes/notes.state';
 
 export default function Home() {
   const [title, setTitle] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const noteStore = useNoteStore();
 
   // ノート作成ボタンを押したときに実行する関数。API通信があるからasync
   const createNote = async () => {
@@ -20,6 +22,8 @@ export default function Home() {
     try {
       // 入力されたtitleを使いノード作成APIを呼ぶ。
       const newNote = await noteRepository.create({ title });
+      // noteRepository.create({ title }で作成されたnewNoteをグローバルステートのノート一覧に追加
+      noteStore.set([newNote]);
       // 作成に成功したら入力欄を空に戻す
       setTitle('');
       console.log(newNote);
