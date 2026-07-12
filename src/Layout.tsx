3beab992f113619b514cd8609489bacb9f1d197a
hidebou0113 <wakakinhidesnow920@gmail.com>
2026-07-12 16:40:39 +0900

    レイアウト表示時にノート一覧を読み込むようにした

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,17 +4,40 @@ import './styles/layout.css';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
 import { currentUserAtom } from './modules/auth/current-user.state';
+import { useEffect, useState } from 'react';
+import { useNoteStore } from './modules/notes/notes.state';
+import { noteRepository } from './modules/notes/note.repository';
 
 export default function Layout() {
   // ログイン中のユーザー情報を読み取る
   const currentUser = useAtomValue(currentUserAtom);
+  // ノート一端を取得中かどうか
+  const [isLoading, setIsLoading] = useState(false);
+  // ノート一覧のグローバツステートを操作
+  const noteStore = useNoteStore();
+
+  // レイアウトが表維持された最初のタイミングでfetchNotes()を1回実行
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    // 読み込み中
+    setIsLoading(true);
+    // /notesからノート一覧を取得
+    const notes = await noteRepository.find();
+    // 取得したノート一覧をグローバルステートぬ保存
+    noteStore.set(notes);
+    setIsLoading(false);
+  };
 
   //ログインユーザーがいなければ、メイン画面を表示せず/signinに移動。
   if (!currentUser) return <Navigate to="/signin" replace />;
 
   return (
     <div className="layout-container">
-      <SideBar />
+      {/* ノート一覧の取得中はサイドバーを非表示 */}
+      {!isLoading && <SideBar />}
       <main className="layout-main">
         <Outlet />
       </main>
--- src/modules/notes/note.repository.ts
+++ src/modules/notes/note.repository.ts
@@ -2,6 +2,12 @@ import api from '../../lib/api';
 import { Note } from './note.entity';
 
 export const noteRepository = {
+  // ノート一覧取得APIを呼び出す
+  async find(): Promise<Note[]> {
+    // サーバーからノート一覧を取得し、返ってきたresult.data.notesを1件ずつnew Note(data)に変換
+    const result = await api.get('/notes');
+    return result.data.notes.map((data: Note) => new Note(data));
+  },
   // note新規作成メソッド。paramsにはtitleとparentIdを渡せる。Promise<Note>はNoteを返す非同期処理
   async create(params: { title?: string; parentId?: number }): Promise<Note> {
     // /notesにpostリクエストを送っている。noteを作成して下しとサーバーに依頼。
