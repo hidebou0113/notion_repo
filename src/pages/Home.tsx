2a3c115746f97d91c35e7cb26242700b79d7ae66
hidebou0113 <wakakinhidesnow920@gmail.com>
2026-07-12 14:44:43 +0900

    ホーム画面にノート作成処理を追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,31 @@ import {
 } from '../components/ui/card';
 import { FiPlus } from 'react-icons/fi';
 import '../styles/pages/home.css';
+import { useState } from 'react';
+import { noteRepository } from '../modules/notes/note.repository';
 
 export default function Home() {
+  const [title, setTitle] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // ノート作成ボタンを押したときに実行する関数。API通信があるからasync
+  const createNote = async () => {
+    // 作成処理を開始したので送信中状態をtrue
+    setIsSubmitting(true);
+    try {
+      // 入力されたtitleを使いノード作成APIを呼ぶ。
+      const newNote = await noteRepository.create({ title });
+      // 作成に成功したら入力欄を空に戻す
+      setTitle('');
+      console.log(newNote);
+    } catch (error) {
+      console.error(error);
+      alert('ノートの作成に失敗しました');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <Card className="home-card">
       <CardHeader className="home-card-header">
@@ -21,9 +44,14 @@ export default function Home() {
             className="home-input"
             placeholder="ノートのタイトルを入力"
             type="text"
-            onChange={() => {}}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
-          <button className="home-button" onClick={() => {}}>
+          <button
+            className="home-button"
+            onClick={createNote}
+            disabled={isSubmitting}
+          >
             <FiPlus size={16} />
             <span>ノート作成</span>
           </button>
