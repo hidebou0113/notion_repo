26a30b07a9791317e511c201f0c2e47b16c36671
hidebou0113 <wakakinhidesnow920@gmail.com>
2026-07-12 16:22:00 +0900

    サイドバーからノートを作成する処理を追加

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,8 +2,24 @@ import Item from './Item';
 import NoteList from '../NoteList';
 import UserItem from './UserItem';
 import { FiPlus, FiSearch } from 'react-icons/fi';
+import { useNoteStore } from '../../modules/notes/notes.state';
+import { noteRepository } from '../../modules/notes/note.repository';
 
 export default function SideBar() {
+  // ノート一覧を操作するためのnoteStoreを取得
+  const noteStore = useNoteStore();
+  // ノート作成を押したときに実行される関数
+  const createNote = async () => {
+    try {
+      // ノート作成APIを呼んでいる。{}を藁しているので、タイトルなし、親ノートなしの新規ノートを作る
+      const newNote = await noteRepository.create({});
+      // 作成されたノートをグローバルステートのノート一覧に追加。
+      noteStore.set([newNote]);
+    } catch (error) {
+      console.error(error);
+      alert('ノートの作成に失敗しました');
+    }
+  };
   return (
     <>
       <aside className="sidebar">
@@ -14,7 +30,7 @@ export default function SideBar() {
           </div>
           <div className="sidebar-spacer">
             <NoteList />
-            <Item label="ノートを作成" icon={FiPlus} onClick={() => {}} />
+            <Item label="ノートを作成" icon={FiPlus} onClick={createNote} />
           </div>
         </div>
       </aside>
